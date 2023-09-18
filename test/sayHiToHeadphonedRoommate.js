861ed61c5ea86f19475a7098dc141ea9daedc714
Ruth <ruthmusyoka2000@gmail.com>
2023-09-18 10:22:03 +0300

    assignment completed

---
+++ test/sayHiToHeadphonedRoommate.js
@@ -0,0 +1,7 @@
+function sayHiToHeadphonedRoommate(string) {
+  if (string.toLowerCase() === string) {
+    return "I can\'t hear you!";
+  } else if 
+
+}
+exports.sayHiToHeadphonedRoommate = sayHiToHeadphonedRoommate;
